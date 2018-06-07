9b7b8620a65dd10c3eec796b6d8a01111eeb8107
iaean <iaean@users.noreply.github.com>
2018-06-07 12:03:31 +0200

    Document some API endpoints...

--- examples/api.js
+++ examples/api.js
@@ -9,9 +9,46 @@ var api = new GhostAPI({
   password: 'foobarbaz1' });
 
 api.login(); //.catch(function(e) {console.error(e);});
-api.request('/themes/').then(function(json) { console.log(JSON.stringify(json)); });
+api.request('/themes/').then(function(json) { console.log(JSON.stringify(json, null, 2)); });
 api.logout(); //.catch(function(e) {console.error(e);});
 
 // Export content: /db/?access_token=..
+
+// db[0].data:
+//  "posts": [
+//    {
+//      "feature_image": "https://...",
+//      "og_image": null, /* Facebook? */
+//      "twitter_image": null,
+//  "users": [
+//    {
+//      "profile_image": null,
+//      "cover_image": null,
+//  "settings": [
+//    {
+//      "key": "logo",
+//      "value": "https://casper.ghost.org/v1.0.0/images/ghost-logo.svg",
+//    {
+//      "key": "cover_image",
+//      "value": "https://casper.ghost.org/v1.0.0/images/blog-cover.jpg",
+//    {
+//      "key": "icon",
+//      "value": "",
+//  "tags": [
+//    {
+//      "feature_image": null,
+
 // List themes: /themes/
+
+// {
+//   "themes": [
+//    {
+//      "name": "casper",
+//      // "package": {...},
+//      "active": true,
+//      "templates": []
+//    }
+//  ]
+// }
+
 // Download theme: /themes/casper/download/?access_token=..
